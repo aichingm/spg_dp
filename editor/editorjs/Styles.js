0169eba2067f4fb6385bddde9c87c9eb89011f45
Mario Aichinger <aichingm@gmail.com>
2015-05-09 20:32:16 +0200

    add new editor style "GoodStyles"

--- editor/editorjs/Styles.js
+++ editor/editorjs/Styles.js
@@ -1,115 +1,108 @@
 var Styles = {
-	floor : {
-		"color" : "rgba(30,30,30,0.750)"
-	},
-	wall : {
-		"width" : 6,
-		"color" : "#85144B"
-	},
-	edge : {
-		"width" : 2,
-		"color" : "#FF3399"
-	},
-	window : {
-		"width" : 2,
-		"color" : "#FF851B"
-	},
-	door : {
-		"width" : 3,
-		"color" : "#01FF70"
-	},
-	pathPoint : {
-		"color" : "#309900",
-		"lineWidth" : 3,
-		"strokeStyle" : "#000000",
-		"pointRadius" : 5
-	},
-	selectedPoint : {
-		"color" : "#FF4136",
-		"lineWidth" : 3,
-		"strokeStyle" : "#000000",
-		"pointRadius" : 5
-	},
-	point : {
-		"color" : "#3D9970",
-		"lineWidth" : 3,
-		"strokeStyle" : "#000",
-		"pointRadius" : 5
-	},
-	vertexPoint : {
-		"color" : "rgba(236,247,30,0.75)",
-		"pointRadius" : 10
-	},
-	vertexSelectedPoint : {
-		"color" : "rgba(236,247,30,0.75)",
-		"pointRadius" : 10,
-		"lineWidth" : 3,
-		"strokeStyle" : "#000"
-	},
-	vertexSelectedPointA : {
-		"color" : "rgba(0, 0, 204, 1)",
-		"pointRadius" : 10
-	},
-	vertexSelectedPointB : {
-		"color" : "rgba(0, 204, 0,1)",
-		"pointRadius" : 10
-	}
-}
+    floor: {
+        "color": "rgba(30,30,30,0.75)"
+    },
+    wall: {
+        "width": 6,
+        "color": "#85144B"
+    },
+    edge: {
+        "width": 2,
+        "color": "#FF3399"
+    },
+    window: {
+        "width": 2,
+        "color": "#FF851B"
+    },
+    door: {
+        "width": 3,
+        "color": "#01FF70"
+    },
+    pathPoint: {
+        "color": "#309900",
+        "lineWidth": 3,
+        "strokeStyle": "#000000",
+        "pointRadius": 5
+    },
+    selectedPoint: {
+        "color": "#FF4136",
+        "lineWidth": 3,
+        "strokeStyle": "#000000",
+        "pointRadius": 5
+    },
+    point: {
+        "color": "#3D9970",
+        "lineWidth": 3,
+        "strokeStyle": "#000",
+        "pointRadius": 5
+    },
+    vertexPoint: {
+        "color": "rgba(236,247,30,0.75)",
+        "pointRadius": 10
+    },
+    vertexSelectedPoint: {
+        "color": "rgba(236,247,30,0.75)",
+        "pointRadius": 10,
+        "lineWidth": 3,
+        "strokeStyle": "#000"
+    },
+    vertexSelectedPointA: {
+        "color": "rgba(0, 0, 204, 1)",
+        "pointRadius": 10
+    },
+    vertexSelectedPointB: {
+        "color": "rgba(0, 204, 0,1)",
+        "pointRadius": 10
+    }
+};
 
 var GoodStyles = {
-		floor : {
-			"color" : "rgba(60,30,100,0.750)"
-		},
-		wall : {
-			"width" : 1,
-			"color" : "#851A4B"
-		},
-		edge : {
-			"width" : 2,
-			"color" : "#FF3399"
-		},
-		window : {
-			"width" : 2,
-			"color" : "#FF851B"
-		},
-		door : {
-			"width" : 3,
-			"color" : "#01FF70"
-		},
-		pathPoint : {
-			"color" : "#309900",
-			"lineWidth" : 3,
-			"strokeStyle" : "#000000",
-			"pointRadius" : 5
-		},
-		selectedPoint : {
-			"color" : "#FF4136",
-			"lineWidth" : 3,
-			"strokeStyle" : "#000000",
-			"pointRadius" : 5
-		},
-		point : {
-			"color" : "#3D9970",
-			"lineWidth" : 3,
-			"strokeStyle" : "#000",
-			"pointRadius" : 5
-		},
-		vertexPoint : {
-			"color" : "rgba(236,247,30,0.75)",
-			"pointRadius" : 10
-		},
-		vertexSelectedPoint : {
-			"color" : "rgba(236,247,30,0.75)",
-			"pointRadius" : 10,
-			"lineWidth" : 3,
-			"strokeStyle" : "#000"
-		},
-		vertexSelectedPointA : {
-			"color" : "rgba(0, 0, 204, 1)",
-			"pointRadius" : 10
-		},
-		vertexSelectedPointB : {
-			"color" : "rgba(0, 204, 0,1)",
-			"pointRadius" : 10
-		}
-	}
+    floor: {
+        "color": "rgba(0,31,63,0.75)"
+    },
+    wall: {
+        "width": 5,
+        "color": "#FFDC00"
+    },
+    edge: {
+        "width": 5,
+        "color": "#B10DC9"
+    },
+    window: {
+        "width": 5,
+        "color": "#FF851B"
+    },
+    door: {
+        "width": 5,
+        "color": "#FF4136"
+    },
+    selectedPoint: {
+        "color": "#7FDBFF",
+        "pointRadius": 8
+    },
+    point: {
+        "color": "#0074D9",
+        "pointRadius": 8
+    },
+    vertexPoint: {
+        //"color" : "rgba(240,18,190,0.75)",
+        "color": "#85144b",
+        "pointRadius": 10
+    },
+    vertexSelectedPoint: {
+        "color": "#F012BE",
+        //"color" : "rgba(236,247,30,0.75)",
+        "pointRadius": 10
+
+    },
+    vertexSelectedPointA: {
+        //"color": "rgba(0, 0, 204, 1)",
+        "color": "#F012BE",
+        "pointRadius": 10
+    },
+    vertexSelectedPointB: {
+        //"color": "rgba(0, 204, 0,1)",
+        "color": "#01FF70",
+        "pointRadius": 10
+    }
+};
