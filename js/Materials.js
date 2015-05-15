05b0f029371c94f6c0ced723ecc2780cb8ca3495
Mario Aichinger <aichingm@gmail.com>
2015-05-15 15:11:49 +0200

    refactor Materials

---
+++ js/Materials.js
@@ -0,0 +1,24 @@
+
+var windowMaterial = new THREE.MeshBasicMaterial({
+    color: 0x0030ee,
+    opacity: 0.5,
+    transparent: true,
+    side: THREE.DoubleSide
+});
+var wallMaterial = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    opacity: 0.5,
+    transparent: true,
+    side: THREE.DoubleSide
+});
+var doorMaterial = new THREE.MeshBasicMaterial({
+    color: 0xee00ee,
+    opacity: 0.5,
+    transparent: true,
+    side: THREE.DoubleSide
+});
+var floorMaterial = new THREE.MeshBasicMaterial({
+    color: 0x555555,
+    transparent: false,
+    side: THREE.DoubleSide
+});
