e3849d7fccaba18bc7a97ad17ba6dc9254747e59
Mario Aichinger <aichingm@gmail.com>
2015-05-16 20:21:58 +0200

    fix error handling on empty paths or vertices

--- js/events/on..leftDrawer.route.leftDeawer-show.js
+++ js/events/on..leftDrawer.route.leftDeawer-show.js
@@ -1,7 +1,20 @@
 $(document).ready(function (e) {
     $(".leftDrawer.route").on("leftDrawer-open", function (e) {
-        var exports = localStorage.getItem("PieceofShit.exports");
-        var list = buildVetexNameList(JSON.parse(exports).paths);
+        var list, exports = localStorage.getItem("PieceofShit.exports");
+        exports = JSON.parse(exports);
+        console.log(exports && exports.paths)
+        if (exports && exports.paths && exports.paths.vertices.length > 0) {
+            list = buildVetexNameList(exports.paths);
+            $(".leftDrawer.route .content:first-child").show();
+            $(".leftDrawer.route .content:last-child").hide();
+
+        } else {
+            //exports or exports.paths are unset so simply show an error and return. don't setup anything
+            $(".leftDrawer.route .content:first-child").hide();
+            $(".leftDrawer.route .content:last-child").show();
+            return;
+        }
+
         $(".leftDrawer.route div select[name='from']").html("<option value=\"-1\">from</option>");
         $(".leftDrawer.route div select[name='to']").html("<option value=\"-1\">to</option>");
         for (var i = 0; i < list.length; i++) {
@@ -29,7 +42,7 @@ $(document).ready(function (e) {
         var map = buildMapFromDataPaths(exports.paths);
         graph = new Dijkstra(map);
         var path = graph.getPath($(".leftDrawer.route div select[name='from']").val(), $(".leftDrawer.route div select[name='to']").val());
-        
+
         VIEWER.setPath(path);
         VIEWER.draw(true);
         $drawer = $(".leftDrawer.isIn");
