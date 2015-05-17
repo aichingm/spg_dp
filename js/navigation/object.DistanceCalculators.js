f2977c5a225f8f88561e81a42aab31eb891f2328
Mario Aichinger <aichingm@gmail.com>
2015-05-17 10:21:57 +0200

    add new distance calculator 3D-distance times metric

--- js/navigation/object.DistanceCalculators.js
+++ js/navigation/object.DistanceCalculators.js
@@ -14,7 +14,14 @@ var DistanceCalculators = {
         bZ = data.modelManager.floors[b.floorIndex].offset.z;
         vac = new THREE.Vector3(a.x, a.y, aZ);
         d = vac.distanceTo(new THREE.Vector3(b.x, b.y, bZ));
-        console.log(d)
         return d;
+    },
+    calculateByVectorDistance3DTimesMetric: function (direction, edge, a, b, data) {
+        var aZ, bZ, vac, d;
+        aZ = data.modelManager.floors[a.floorIndex].offset.z;
+        bZ = data.modelManager.floors[b.floorIndex].offset.z;
+        vac = new THREE.Vector3(a.x, a.y, aZ);
+        d = vac.distanceTo(new THREE.Vector3(b.x, b.y, bZ));
+        return d * edge.metric[direction];
     }
 };
