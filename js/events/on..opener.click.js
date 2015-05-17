5d2975d281e4dea99b864177481edae9ce6ac6ab
Mario Aichinger <aichingm@gmail.com>
2015-05-17 15:13:46 +0200

    add jQuery events to the left drawer event system

--- js/events/on..opener.click.js
+++ js/events/on..opener.click.js
@@ -2,31 +2,38 @@ $(document).ready(function () {
     $(".opener").click(function () {
         var index = $(this).index() + 1;
         $drawer = $(".leftDrawer:nth-child(" + index + ")");
-        $drawer.trigger("leftDrawer-open");
-        $drawer.animate({
-            left: "0%"
-        }, {
-            duration: 200,
-            done: function () {
-                $drawer.trigger("leftDrawer-opened");
-            }
-        }).addClass("isIn");
-        $(this).animate({
-            "margin-left": $drawer.width()
-        }, 200);
+        var event = jQuery.Event("leftDrawer-open");
+        $drawer.trigger(event);
+        if (!event.isDefaultPrevented()) {
+            $drawer.animate({
+                left: "0%"
+            }, {
+                duration: 200,
+                done: function () {
+                    $drawer.trigger("leftDrawer-opened");
+                }
+            }).addClass("isIn");
+            $(this).animate({
+                "margin-left": $drawer.width()
+            }, 200);
+        }
     });
     $(".leftDrawer div button.cancel").click(function () {
         $drawer = $(".leftDrawer.isIn");
-        $drawer.trigger("leftDrawer-close")
-                .removeClass("isIn")
-                .animate({
-                    left: "-100%"
-                }, {
-                    duration: 200,
-                    done: function () {
-                        $drawer.trigger("leftDrawer-closed");
-                    }
-                });
+        var event = jQuery.Event("leftDrawer-close");
+        $drawer.trigger(event);
+        if (!event.isDefaultPrevented()) {
+            $drawer.removeClass("isIn")
+                    .animate({
+                        left: "-100%"
+                    }, {
+                        duration: 200,
+                        done: function () {
+                            var event = jQuery.Event("leftDrawer-closed");
+                            $drawer.trigger(event);
+                        }
+                    });
+        }
         //animates all .opener to margin-left: 0 which is preatty dirty!! fix this todo
         $(".opener").animate({
             "margin-left": 0
@@ -35,7 +42,10 @@ $(document).ready(function () {
 
     $(".leftDrawer div button.apply").click(function () {
         $drawer = $(".leftDrawer.isIn");
-        $drawer.trigger("leftDrawer-apply");
-        $(".leftDrawer.isIn div button.cancel").trigger("click");
+        var event = jQuery.Event("leftDrawer-apply");
+        $drawer.trigger(event);
+        if (!event.isDefaultPrevented()) {
+            $(".leftDrawer.isIn div button.cancel").trigger("click");
+        }
     });
 });
